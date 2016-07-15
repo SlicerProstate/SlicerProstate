fa9fad7cff1b62b9597da7cb53a55d7a4dd3cfc1
Christian Herz <christianherz1985@gmail.com>
2016-07-15 11:50:16 -0400

    BUG: adding parameters for specifying dilateValue and erodeValue (issue #21): - also changed kernel size

--- SlicerProstate/SlicerProstateUtils/mixins.py
+++ SlicerProstate/SlicerProstateUtils/mixins.py
@@ -246,13 +246,13 @@ class ModuleLogicMixin(object):
     return distance3D
 
   @staticmethod
-  def dilateMask(label):
+  def dilateMask(label, dilateValue, erodeValue=0):
     imagedata = label.GetImageData()
     dilateErode = vtk.vtkImageDilateErode3D()
     dilateErode.SetInputData(imagedata)
-    dilateErode.SetDilateValue(1.0)
-    dilateErode.SetErodeValue(0.0)
-    dilateErode.SetKernelSize(12, 12, 1)
+    dilateErode.SetDilateValue(dilateValue)
+    dilateErode.SetErodeValue(erodeValue)
+    dilateErode.SetKernelSize(3, 3, 3)
     dilateErode.Update()
     label.SetAndObserveImageData(dilateErode.GetOutput())
 
