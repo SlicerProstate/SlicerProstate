ad8093daea0fb73c9e1ca329b3e052b7a42aea65
Christian Herz <christianherz1985@gmail.com>
2016-08-18 20:42:29 -0400

    ENH: implemented SampleDownloader, that allows users to cancel the running download - inherited from FancyURLopener and override retrieve method for enabling cancellation - moved some events from SliceTracker to SlicerProstate

--- SlicerProstate/SlicerProstateUtils/events.py
+++ SlicerProstate/SlicerProstateUtils/events.py
@@ -2,6 +2,9 @@ import vtk
 
 class SlicerProstateEvents(object):
 
+  NewImageDataReceivedEvent = vtk.vtkCommand.UserEvent + 100
+  NewFileIndexedEvent = vtk.vtkCommand.UserEvent + 101
+
   IncomingDataSkippedEvent = vtk.vtkCommand.UserEvent + 200
   IncomingDataCanceledEvent = vtk.vtkCommand.UserEvent + 201
   IncomingDataReceiveFinishedEvent = vtk.vtkCommand.UserEvent + 202
@@ -9,6 +12,10 @@ class SlicerProstateEvents(object):
   DICOMReceiverStartedEvent = vtk.vtkCommand.UserEvent + 204
   DICOMReceiverStoppedEvent = vtk.vtkCommand.UserEvent + 205
 
-  StatusChangedEvent = vtk.vtkCommand.UserEvent + 203
+  StatusChangedEvent = vtk.vtkCommand.UserEvent + 206
 
-  RatingWindowClosedEvent = vtk.vtkCommand.UserEvent + 204
+  RatingWindowClosedEvent = vtk.vtkCommand.UserEvent + 304
+
+  DownloadCanceledEvent = vtk.vtkCommand.UserEvent + 401
+  DownloadFinishedEvent = vtk.vtkCommand.UserEvent + 402
+  DownloadFailedEvent = vtk.vtkCommand.UserEvent + 403
